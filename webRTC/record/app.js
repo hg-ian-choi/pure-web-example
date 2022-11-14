e572c58d447cf396459fb9fb70c02fa495caa667
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-14 23:36:51 +0900

    webRTC: modify "capture" function

--- webRTC/record/app.js
+++ webRTC/record/app.js
@@ -1,9 +1,11 @@
 let stream;
 
 const openCamButton = document.querySelector('button#openCamButton');
+const captureButton = document.querySelector('button#captureButton');
+const preview = document.querySelector('video#preview');
+const capture = document.querySelector('canvas#capture');
 
 async function openCam(type) {
-  let video = document.querySelector('video#preview');
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices
       .getUserMedia({
@@ -11,15 +13,26 @@ async function openCam(type) {
         video: { facingMode: type },
       })
       .then((_stream) => {
-        if ('srcObject' in video) {
-          video.srcObject = _stream;
-          video.play();
+        if ('srcObject' in preview) {
+          preview.srcObject = _stream;
+          preview.play();
           stream = _stream;
+          preview.style.display = 'block';
+          captureButton.disabled = false;
         }
       });
   }
 }
 
+async function onCapture() {
+  const context = capture.getContext('2d');
+  context.drawImage(preview, 0, 0, 300, 225);
+  capture.style.display = 'block';
+}
+
 openCamButton.addEventListener('click', async function () {
   await openCam('user');
+});
+captureButton.addEventListener('click', async function () {
+  await onCapture();
 });
